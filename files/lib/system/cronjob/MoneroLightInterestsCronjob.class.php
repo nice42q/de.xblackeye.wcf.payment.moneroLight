f7060b40fa2ef7a467a50b00c14e8370eac0e64c
Luke <17616949+nice42q@users.noreply.github.com>
2023-08-21 13:57:34 +0200

    Add files via upload

---
+++ files/lib/system/cronjob/MoneroLightInterestsCronjob.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author  xBlackEye
+ * @license
+ * @package de.xblackeye.wcf.payment.moneroLight
+ */
+namespace wcf\system\cronjob;
+
+use wcf\data\cronjob\Cronjob;
+use wcf\data\user\UserEditor;
+use wcf\data\user\UserProfileList;
+use wcf\system\WCF;
+
+/**
+ * Cronjob for Monero Interests
+ */
+class MoneroLightInterestsCronjob extends AbstractCronjob
+{
+    /**
+     * @inheritdoc
+     */
+    public function execute(Cronjob $cronjob)
+    {}
+}
